aedeae6647263f3c09e20f98bf1bb63b6d84a4a9
harshal-ganbote <harshal.ganbote@outlook.com>
2024-09-26 06:27:35 +0530

    builder pattern done

---
+++ L4_Design_Patterns_Builder/51149000-builder-configuration/src/main/java/com/assignment/question/DatabaseConfigurationBuilder.java
@@ -0,0 +1,88 @@
+package com.assignment.question;
+
+@WithBuilder
+public class DatabaseConfigurationBuilder {
+  private String databaseUrl;
+  private String username;
+  private String password;
+  private int maxConnections;
+  private boolean enableCache;
+  private boolean isReadOnly;
+
+  private DatabaseConfigurationBuilder(Builder builder) {
+    this.databaseUrl = builder.databaseUrl;
+    this.username = builder.username;
+    this.password = builder.password;
+    this.maxConnections = builder.maxConnections;
+    this.enableCache = builder.enableCache;
+    this.isReadOnly = builder.isReadOnly;
+  }
+
+  public String getDatabaseUrl() {
+    return databaseUrl;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public int getMaxConnections() {
+    return maxConnections;
+  }
+
+  public boolean isEnableCache() {
+    return enableCache;
+  }
+
+  public boolean isReadOnly() {
+    return isReadOnly;
+  }
+
+  static class Builder {
+    private String databaseUrl;
+    private String username;
+    private String password;
+    private int maxConnections;
+    private boolean enableCache;
+    private boolean isReadOnly;
+
+    public Builder setDatabaseUrl(String databaseUrl) {
+      this.databaseUrl = databaseUrl;
+      return this;
+    }
+
+    public Builder setUsername(String username) {
+      this.username = username;
+      return this;
+    }
+
+    public Builder setPassword(String password) {
+      this.password = password;
+      return this;
+    }
+
+    public Builder setMaxConnections(int maxConnections) {
+      this.maxConnections = maxConnections;
+      return this;
+    }
+
+    public Builder setEnableCache(boolean enableCache) {
+      this.enableCache = enableCache;
+      return this;
+    }
+
+    public Builder setIsReadOnly(boolean isReadOnly) {
+      this.isReadOnly = isReadOnly;
+      return this;
+    }
+
+    public DatabaseConfiguration build() {
+      return new DatabaseConfiguration(this);
+    }
+
+  }
+}
